fe10a46239fe5ebad8e9f884dbb9eec3811bf723
take32457 <take32457@gmail.com>
2023-02-26 14:49:01 +0900

    view chirp

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -17,7 +17,7 @@ class ChirpController extends Controller
     public function index(): Response 
     {
         return Inertia::render('Chirps/Index', [
-            //
+            'chirps' => Chirp::with('user:id,name')->latest()->get(),
         ]);
     }
 
